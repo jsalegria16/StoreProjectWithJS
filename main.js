2c45c8963a07e80e094601161c7a542292d07d00
JhonAlegria_Git <alegriasebas.sa@hotmail.com>
2023-03-04 15:59:47 -0500

    Integracion del menu mobile con JS

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const menuDEsktop = document.querySelector('.NavBarEmail'); //PAra eventgos sobre el email de navRight
 const DesktopMenucontainer = document.querySelector('.DesktopMenucontainer')
 
+const menuButtMobile = document.querySelector('.MenuIcon'); //PAra eventgos sobre el icono de menu en mobile
+const MEnuMobileContainer = document.querySelector('.MobileMenu')
 
 
 menuDEsktop.addEventListener('click',toggleDesktopMenu);
-
 function toggleDesktopMenu(){
-
     DesktopMenucontainer.classList.toggle('MenuInactive');
+}
 
-
+menuButtMobile.addEventListener('click',toggleMobileMenu);
+function toggleMobileMenu(){
+    MEnuMobileContainer.classList.toggle('MenuInactive');
 }

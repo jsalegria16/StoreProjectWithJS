560e1ace58a449eb0521d5f023f75387a4343327
JhonAlegria_Git <alegriasebas.sa@hotmail.com>
2023-03-07 15:42:34 -0500

    Dettale del producto, cerrar detalle del producto y shoopingcar y responsive para mobile en product detail y shopping car

--- main.js
+++ main.js
@@ -6,14 +6,19 @@ const MEnuMobileContainer = document.querySelector('.MobileMenu')
 
 const ShoppingCarIconMenu = document.querySelector('.ShoppingCarNavBar'); //PAra eventgos sobre el carrito de compra
 const ShoppingCar = document.querySelector('.ShoppingCar')
+const shoppingCarCloseIcon = document.querySelector('.shoppingCarCloseIcon'); //Cerrar el porductDEtail
 
 const CardsContainer =  document.querySelector(".CardsContainer") //PAra la lista de productos(InitialPage)
 const productList = []; //Para la Lista de productos
+
+const ProductDetail = document.querySelector('.ProductDetail'); //PAra para los detalles del producto
+const productDetailcloseIcon = document.querySelector('.productDetailcloseIcon'); //Cerrar el porductDEtail
 
 menuDEsktop.addEventListener('click',toggleDesktopMenu);
 function toggleDesktopMenu(){
 
     ShoppingCar.classList.add('MenuInactive');
+    ProductDetail.classList.add('MenuInactive');
     DesktopMenucontainer.classList.toggle('MenuInactive');
 
 }
@@ -21,6 +26,7 @@ function toggleDesktopMenu(){
 menuButtMobile.addEventListener('click',toggleMobileMenu);
 function toggleMobileMenu(){
     ShoppingCar.classList.add('MenuInactive');
+    ProductDetail.classList.add('MenuInactive');
     MEnuMobileContainer.classList.toggle('MenuInactive');
 }
 
@@ -28,8 +34,27 @@ ShoppingCarIconMenu.addEventListener('click',toggleShoppingCar);
 function toggleShoppingCar(){
     DesktopMenucontainer.classList.add('MenuInactive');
     MEnuMobileContainer.classList.add('MenuInactive');
+    ProductDetail.classList.add('MenuInactive');
     ShoppingCar.classList.toggle('MenuInactive');
 }
+shoppingCarCloseIcon.addEventListener('click',CloseShoppingCar);
+function CloseShoppingCar(){
+    ShoppingCar.classList.add('MenuInactive');
+}
+
+function OpenProductDetail(){
+    DesktopMenucontainer.classList.add('MenuInactive');
+    MEnuMobileContainer.classList.add('MenuInactive');
+    ShoppingCar.classList.add('MenuInactive');
+    ProductDetail.classList.remove('MenuInactive');
+}
+
+productDetailcloseIcon.addEventListener('click',CloseProductDetail);
+function CloseProductDetail(){
+    ProductDetail.classList.add('MenuInactive');
+}
+
+
 
 /* Ahora mavos a crear un array de los diferentes productos */
 
@@ -58,7 +83,6 @@ function createListProducts(){
         imageProduct:'./icons/Product11.png'
     });
 }
-
 function RenderElemnts() {
     for (eachproduct of productList) {
         const maindiv = document.createElement('div')
@@ -89,15 +113,21 @@ function RenderElemnts() {
             subDivProduct.append(DivOf_subDivProduct,FigureOf_SubDivProduct);
     
         maindiv.append(subImg,subDivProduct);
+
+        maindiv.addEventListener('click',OpenProductDetail); //Para Mirar los detalles del procucto
     
         CardsContainer.append(maindiv); //mando esto para .Cardcontainer del HTML
     
     }
 }
-
 createListProducts();
 RenderElemnts() ;
-
 //for (eachproduct in productList) { >> ME muestra el índise
-
 /*Finaliza Ahora mavos a crear un array de los diferentes productos */
+
+/* PAra mostrar los detalles de un producto */
+
+
+
+
+/* Finaliza PAra mostrar los detalles de un producto */
